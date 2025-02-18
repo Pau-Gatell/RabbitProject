d494d5011927bb25446d4e52e5527fbe497f3bff
Pau-Gatell <455837.clot@fje.edu>
2025-02-18 09:30:25 +0100

    FixCamera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
+    public Vector3 offset; // Desplazamiento de la cámara
 
     void Start()
     {
-
+        // Ejemplo de desplazamiento: 2 unidades a la derecha y 1 arriba
+        offset = new Vector3(20f, 24f, 0f);
     }
 
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(
+            player.transform.position.x + offset.x,
+            player.transform.position.y + offset.y,
+            transform.position.z
+        );
     }
 }
 
