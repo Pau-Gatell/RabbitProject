33fc86587adc4579afed99505f161a13eaad238c
Pau-Gatell <455837.clot@fje.edu>
2025-02-27 09:17:34 +0100

    Delete part of tile map and change scene script

---
+++ Assets/Scripts/CambioDeEscena.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscena2D : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+    
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+}
